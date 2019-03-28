4db9c0265051e3d86a3293a2ea698fb7e4a7d750
baiyanquan <405579949@qq.com>
2019-03-28 14:54:34 +0800

    根据新建的数据库与之前数据库的差异，进行后端代码的整理，使后端可以在现在的数据库上正确运行

--- src/main/java/com/example/now/repository/QuestionResourceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.now.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.now.entity.QuestionResource;
-import java.util.List;
-public interface QuestionResourceRepository extends JpaRepository<QuestionResource,Integer>{
-    List<QuestionResource> findByQuestionId(int questionId);
-}
