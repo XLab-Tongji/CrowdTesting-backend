7141bdc43598387ab81631c8b842d7f0cafdcd9e
Jiaojinchao <314279802@qq.com>
2019-05-05 12:12:22 +0800

    将"/answer/update","/answer/add"两个接口合成为"/answer/update"一个

--- src/main/java/com/example/now/controller/AnswerController.java
+++ src/main/java/com/example/now/controller/AnswerController.java
@@ -2,6 +2,7 @@ package com.example.now.controller;
 
 import com.example.now.entity.IdStore;
 import com.example.now.entity.Answer;
+import com.example.now.repository.AnswerRepository;
 import com.example.now.service.RequesterService;
 import com.example.now.service.AnswerService;
 import com.example.now.entity.ResultMap;
@@ -37,6 +38,8 @@ public class AnswerController {
     private RequesterService requesterService;
     @Autowired
     private WorkerService workerService;
+    @Autowired
+    private AnswerRepository answerRepository;
     @Autowired
     private HttpServletRequest request;
 
@@ -80,7 +83,7 @@ public class AnswerController {
         return new ResultMap().success().data("Answers", result);
     }
 
-    // worker 第一次提交子任务答案
+    // worker 第一次提交子任务答案（弃用）
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public ResultMap answerAdd(int task_id, String answer, Timestamp answer_time,int subtaskId,Integer beginAt,Integer endAt) {
         String authToken = request.getHeader(this.tokenHeader);
@@ -94,15 +97,29 @@ public class AnswerController {
     }
 
     // worker 更新子任务答案
-    @RequestMapping(value = "/update", method = RequestMethod.PUT)
-    public ResultMap answerUpdate(int task_id, String answer, Timestamp answer_time, int id,int subtaskId,Integer beginAt,Integer endAt) {
-        String authToken = request.getHeader(this.tokenHeader);
-        String username = this.tokenUtils.getUsernameFromToken(authToken);
-        String message = answerService.updateAnswer(workerService.findWorkerByUsername(username).getId(),task_id, answer,answer_time,id,subtaskId,beginAt,endAt);
-        if(!message.equals("succeed")){
-            return new ResultMap().fail("400").message("update failed");
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ResultMap answerUpdate(int task_id, String answer, Timestamp answer_time,int subtaskId,Integer beginAt,Integer endAt) {
+        //判断该答案字段是否已存在
+        if(answerRepository.existsBySubtaskId(subtaskId)){//答案字段已存在
+            Answer answer1=answerRepository.findBySubtaskId(subtaskId);
+            String authToken = request.getHeader(this.tokenHeader);
+            String username = this.tokenUtils.getUsernameFromToken(authToken);
+            String message = answerService.updateAnswer(workerService.findWorkerByUsername(username).getId(),task_id, answer,answer_time,answer1.getId(),subtaskId,beginAt,endAt);
+            if(!message.equals("succeed")){
+                return new ResultMap().fail("400").message("update failed");
+            }
+            return new ResultMap().success("201").message(message).data("AnswerId",answer1.getId());
         }
-        return new ResultMap().success("201").message(message);
+        else{//答案字段不存在
+            String authToken = request.getHeader(this.tokenHeader);
+            String username = this.tokenUtils.getUsernameFromToken(authToken);
+            IdStore answerId=new IdStore();
+            String message = answerService.addAnswer(workerService.findWorkerByUsername(username).getId(), task_id,answer,answer_time,answerId,subtaskId,beginAt,endAt);
+            if (!message.equals("succeed")) {
+                return new ResultMap().fail("400").message(message);
+            }
+            return new ResultMap().success("201").message(message).data("AnswerId",answerId.getId());
+        }
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
